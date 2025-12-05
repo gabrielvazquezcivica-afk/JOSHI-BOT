c32787a6227ea801c1a8ad324f16b09f302d02cd
GABO3 <gabrielvazquezcivica@gmail.com>
2025-12-05 17:45:08 -0600

    Update YouTube API key in play2.js

--- plugins/play2.js
+++ plugins/play2.js
@@ -4,7 +4,7 @@ import ytdl from "@distube/ytdl-core";
 import { isCommandEnabled } from "./enable.js";
 
 // ---------- AQUI VA TU API KEY ----------
-const YT_API = "AQUI_TU_API_KEY";
+const YT_API = "AIzaSyBDC1a2MaAyr2DE2qDnN9IVInwkWFZB348";
 // -----------------------------------------
 
 export const playVideoHandler = async (conn, m) => {
